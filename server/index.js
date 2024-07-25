1bcba73b8a3c1427861d1d20fa0fffadd7dfa4aa
thesologlobesightseer-jscoder <thesologlobesightseer@gmail.com>
2024-07-25 19:33:15 +0530

    Default path

--- server/index.js
+++ server/index.js
@@ -37,4 +37,8 @@ app.post("/api", (req, res) => {
     });
 });
 
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+});
+
 app.listen(3333, () => console.log("Server running on port 3333"));
