1353eef3cf6f8bc458403f9bbcb8564d092c5103
thesologlobesightseer-jscoder <thesologlobesightseer@gmail.com>
2024-07-25 20:40:30 +0530

    Interrim CORS

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors")
+
 const app = express();
 const bodyParser = require("body-parser");
+
+app.options("*", cors());
 app.use(bodyParser.json());
 
 const { TextServiceClient } =
