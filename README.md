d543560a657b604421eed59165cfc2630bc86df8
thesologlobesightseer-jscoder <thesologlobesightseer@gmail.com>
2024-07-25 17:53:13 +0530

    Gemini Integration: Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# palm-integration
+#  Building a React Application with Google Bard and the PalM2 API 
+
+## Instructions
+This repository has branches for each of the videos in the course. You can use the branch pop up menu in github to switch to a specific branch and take a look at the course at that stage, or you can add `/tree/BRANCH_NAME` to the URL to go to the branch you want to access.
+
+## Branches
+The `master` branch holds the final state of the code when in the course.
+
+When switching from one exercise files branch to the next after making changes to the files, you may get a message like this:
+
+    error: Your local changes to the following files would be overwritten by checkout:        [files]
+    Please commit your changes or stash them before you switch branches.
+    Aborting
+
+To resolve this issue:
+	
+    Add changes to git using this command: git add .
+	Commit changes using this command: git commit -m "some message"
+
+## Installing
+1. Clone this repository into your local machine using the terminal (Mac), CMD (Windows), or a GUI tool like SourceTree.
+
---
+++ client/src/App.jsx
@@ -0,0 +1,102 @@
+import { useState, useEffect, useRef } from "react";
+import DOMPurify from "dompurify";
+import { marked } from "marked";
+import "./App.css";
+
+marked.use({
+  gfm: true,
+});
+
+function App() {
+  const [results, setResults] = useState([]);
+
+  const [serverData, setServerData] = useState([{}]);
+  const [userPrompt, setUserPrompt] = useState("");
+  const [promptResulted, setPromptResulted] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      inputRef.current.blur();
+      handleSubmit();
+    }
+  }
+
+  const handleSubmit = () => {
+    setLoading(true);
+    setServerData("");
+    setPromptResulted(false);
+    if (userPrompt !== "") {
+      fetch("/api", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ "prompt": userPrompt })
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          const html = DOMPurify.sanitize(marked(data));
+          setServerData({ ...data, html });
+          inputRef.current.focus();
+          setUserPrompt("");
+          setPromptResulted(true);
+
+          setResults([
+            ...results,
+            {
+              "prompt": userPrompt,
+              "answer": html
+            }
+          ]);
+          setLoading(false);
+        });
+    }
+  }
+
+  const onChange = (e) => {
+    setLoading(false);
+    setUserPrompt(e.target.value);
+  }
+
+  return (
+    <main className="main-wrapper">
+      <h1 className="logo">Gemini</h1>
+      <div className="page-content">
+        <div className="content">
+          {
+            (serverData === "") 
+            ? 
+              (userPrompt == "" && !promptResulted
+              ? 
+                "Loading..." 
+              : 
+                `Searching for "${userPrompt}"`) 
+            : 
+              (serverData.html 
+                ? 
+                <>
+                <article className="html-render" dangerouslySetInnerHTML={{ __html: serverData.html }} /> 
+                </>
+                :
+                null
+              )
+          }
+        </div>
+      </div>
+      <div className="footer">
+        <textarea onChange={onChange} onKeyDown={handleKeyDown} ref={inputRef} value={userPrompt} className="content-textarea" placeholder="Please enter prompt here" />
+        <button onClick={handleSubmit} className="submit-button" disabled={loading}>Search</button>
+      </div>
+    </main>
+  );
+}
+
+export default App;
